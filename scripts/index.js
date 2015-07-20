8911ab09614d95d1247dd79a6dcb1245dedb7b6d
Isukanets <isukanets@gmail.com>
2015-07-20 20:48:13 +0300

    Initial

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+$(function(){
+    getWeatherData('ua', dataReceived, showError);
+
+    function dataReceived(data) {
+        var offset = (new Date()).getTimezoneOffset()*60*1000; 
+        var city = data.city.name;
+        var country = data.city.country;
+    
+        var localTime = new Date(data.list[0].dt*1000 - offset); 
+            addWeather(
+                data.list[0].weather[0].icon,
+                moment(localTime).calendar(),	
+                data.list[0].weather[0].description,
+                Math.round(data.list[0].temp.day) + '&deg;C',
+                data.list[0].pressure,
+                data.list[0].humidity,
+                data.list[0].speed
+        );
+        $('#location').html(city + ', <b>' + country + '</b>'); 
+    }
+
+    function addWeather(icon, day, condition, temp, pressure, humidity, wind){            
+        $('#day').html(day);
+        $('#icon').html('<img width="90" height="90" src="icons/'+ icon +'.png" />');
+        $('#temperature').html(temp);
+        $('#condition').html(condition);    
+        $('#pressure').html(pressure);
+        $('#humidity').html(humidity);
+        $('#wind').html(wind);
+    };
+
+    function showError(msg){
+        $('#error').html('Виникла помилка: ' + msg);
+    }
+});
